952ef9b6bc0210d8e556220e7bbde704de886f93
Giovani <giovaniequisini@gmail.com>
2024-05-04 18:13:27 -0300

    fixing sort

--- sorting/bubble_sort.c
+++ sorting/bubble_sort.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-  int unsorted_array[] = {12, 20, 24, 25, 31, 36, 67, 86, 190, 200}, value = 20;
+  int unsorted_array[] = {43, 62, 18, 24, 87, 41, 20, 89, 76, 82};
   int array_size = sizeof(unsorted_array) / sizeof(int);
 
   int i, j, tmp, swap = 0;
